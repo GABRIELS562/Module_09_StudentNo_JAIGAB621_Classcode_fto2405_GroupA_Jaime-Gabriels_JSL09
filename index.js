64ac4dcaaa941f95781e9538a87ef089fefa8bc8
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-08-06 16:27:22 +0200

    bug fix incomplete: tried new url for API

--- index.js
+++ index.js
@@ -40,14 +40,16 @@ setInterval(getCurrentTime, 1000)
 
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-        .then(res => {
+        
+    //Even tried this API, it didn't work http://openweathermap.org
+    .then(res => {
             if (!res.ok) {
                 throw Error("Weather data not available")
             }
             return res.json()
         })
         .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
