6f965850ef4669293cda9911e9350c406153dd42
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-08-06 13:43:48 +0200

    chore: Completed dogecoing styling and price

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         return res.json()
     })
     .then(data => {
-        console.log(data)
+        
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: R${data.market_data.current_price.zar}</p>
+            <p>👆: R${data.market_data.high_24h.zar}</p>
+            <p>👇: R${data.market_data.low_24h.zar}</p>
+        `
     })
     .catch(err => console.error(err))
